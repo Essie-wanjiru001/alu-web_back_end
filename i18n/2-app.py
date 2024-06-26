8b454bbc3fcd5410fe47f150187eec8eb972d67e
Essie-wanjiru001 <esthernjeriwanjiru287@gmail.com>
2024-06-26 20:27:51 +0300

    corrections

--- i18n/2-app.py
+++ i18n/2-app.py
@@ -16,13 +16,13 @@ class Config:
     BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
-app.config.from_object('2-app.Config')
+app.config.from_object(Config)
 
 
 @babel.localeselector
 def get_locale() -> str:
     ''' Determine best match with supported languages '''
-    return request.accept_languages.best_match(['en', 'fr'])
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 @app.route("/", methods=["GET"], strict_slashes=False)
