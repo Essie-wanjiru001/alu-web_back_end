c47a62ce6bf0a6a578f664f51a2cb23d5d8cc50c
Essie-wanjiru001 <esthernjeriwanjiru287@gmail.com>
2024-07-12 21:04:15 +0300

    updates

--- ES6_basic/.eslintrc.js
+++ ES6_basic/.eslintrc.js
@@ -1,21 +1,35 @@
 module.exports = {
   env: {
-    browser: true,
+    browser: false,
     es6: true,
-    node: true,
     jest: true,
   },
   extends: [
-    'eslint:recommended',
-    'plugin:jest/recommended',
+    'airbnb-base',
+    'plugin:jest/all',
   ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
   parserOptions: {
     ecmaVersion: 2018,
     sourceType: 'module',
   },
+  plugins: ['jest'],
   rules: {
     'no-console': 'off',
-    'no-unused-vars': 'warn',
-    'no-undef': 'error',
+    'no-shadow': 'off',
+    'no-restricted-syntax': [
+      'error',
+      'LabeledStatement',
+      'WithStatement',
+    ],
   },
+  overrides:[
+    {
+      files: ['*.js'],
+      excludedFiles: 'babel.config.js',
+    }
+  ]
 };
