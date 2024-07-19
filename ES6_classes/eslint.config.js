8da6f44ff7236bb983e454f1da4ae64a4502c303
Essie-wanjiru001 <esthernjeriwanjiru287@gmail.com>
2024-07-20 01:07:24 +0300

    corrections

--- ES6_classes/eslint.config.js
+++ ES6_classes/eslint.config.js
@@ -1,37 +1,37 @@
-// eslint.config.js
 /** @type {import('eslint').ESLint.Options} */
-const config = [
-  require('eslint-config-airbnb-base'),
-  require('eslint-plugin-jest').configs.all,
-  {
-    languageOptions: {
-      globals: {
-        Atomics: 'readonly',
-        SharedArrayBuffer: 'readonly',
-      },
-      parserOptions: {
-        ecmaVersion: 2018,
-        sourceType: 'module',
-      },
+const config = {
+  languageOptions: {
+    globals: {
+      Atomics: 'readonly',
+      SharedArrayBuffer: 'readonly',
     },
-    rules: {
-      'max-classes-per-file': 'off',
-      'no-underscore-dangle': 'off',
-      'no-console': 'off',
-      'no-shadow': 'off',
-      'no-restricted-syntax': [
-        'error',
-        'LabeledStatement',
-        'WithStatement',
-      ],
+    parserOptions: {
+      ecmaVersion: 2018,
+      sourceType: 'module',
     },
-    overrides: [
-      {
-        files: ['*.js'],
-        excludedFiles: 'babel.config.js',
-      },
+  },
+  extends: [
+    'airbnb-base',
+    'plugin:jest/recommended',
+  ],
+  plugins: ['jest'],
+  rules: {
+    'max-classes-per-file': 'off',
+    'no-underscore-dangle': 'off',
+    'no-console': 'off',
+    'no-shadow': 'off',
+    'no-restricted-syntax': [
+      'error',
+      'LabeledStatement',
+      'WithStatement',
     ],
   },
-];
+  overrides: [
+    {
+      files: ['*.js'],
+      excludedFiles: 'babel.config.js',
+    }
+  ]
+};
 
 module.exports = config;
