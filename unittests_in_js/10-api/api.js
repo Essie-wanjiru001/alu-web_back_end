ab7bb7d85934c5fda23f65ca2767a56b9d6e16eb
Essie-wanjiru001 <esthernjeriwanjiru287@gmail.com>
2024-07-27 21:47:11 +0300

    corrections

--- unittests_in_js/10-api/api.js
+++ unittests_in_js/10-api/api.js
@@ -1,33 +1,29 @@
-const express = require('express');
+const exp = require('express');
 
-const app = express();
+const app = exp();
 const port = 7865;
 
-app.use(express.json());
-
 app.get('/', (req, res) => {
-  res.end('Welcome to the payment system');
-});
-
-app.get('/cart/:id([0-9]+)', (req, res) => {
-  res.end(`Payment methods for cart ${req.params.id}`);
-});
-
+    res.send('Welcome to the payment system')
+})
+app.get('/cart/:id(\\d+)', (req, res) => {
+    const id = req.params.id;
+    res.send(`Payment methods for cart ${id}`);
+})
 app.get('/available_payments', (req, res) => {
-  const obj = {
-    payment_methods: {
-      credit_cards: true,
-      paypal: false
-    }
-  };
-  res.json(obj);
-});
+    res.send({
+        payment_methods: {
+            credit_cards: true,
+            paypal: false
+        }
+    })
+})
 
+app.use(exp.json());
 app.post('/login', (req, res) => {
-  const username = req.body.userName;
-  res.end(`Welcome ${username}`);
-});
-
-app.listen(port, () => {
-  console.log('API available on localhost port 7865');
-});
+    const user_name = req.body ? req.body.userName: ''
+    res.send(`Welcome ${user_name}`);
+}).listen(port, () => {
+    console.log(`API available on localhost port 7865`);
+})
+module.exports = app;
--- unittests_in_js/10-api/api.test.js
+++ unittests_in_js/10-api/api.test.js
@@ -1,33 +1,53 @@
-const express = require('express');
+const request = require('request');
+const { expect } = require('chai');
 
-const app = express();
-const port = 7865;
+describe('API integration test', () => {
+  const API_URL = 'http://localhost:7865';
 
-app.use(express.json());
+  it('GET / returns correct response', (done) => {
+    request.get(`${API_URL}/`, (_err, res, body) => {
+      expect(res.statusCode).to.be.equal(200);
+      expect(body).to.be.equal('Welcome to the payment system');
+      done();
+    });
+  });
 
-app.get('/', (req, res) => {
-  res.end('Welcome to the payment system');
-});
+  it('GET /cart/:id returns correct response for valid :id', (done) => {
+    request.get(`${API_URL}/cart/47`, (_err, res, body) => {
+      expect(res.statusCode).to.be.equal(200);
+      expect(body).to.be.equal('Payment methods for cart 47');
+      done();
+    });
+  });
 
-app.get('/cart/:id([0-9]+)', (req, res) => {
-  res.end(`Payment methods for cart ${req.params.id}`);
-});
+  it('GET /cart/:id returns 404 response for negative number values in :id', (done) => {
+    request.get(`${API_URL}/cart/-47`, (_err, res, _body) => {
+      expect(res.statusCode).to.be.equal(404);
+      done();
+    });
+  });
 
-app.get('/available_payments', (req, res) => {
-  const obj = {
-    payment_methods: {
-      credit_cards: true,
-      paypal: false
-    }
-  };
-  res.json(obj);
-});
+  it('GET /cart/:id returns 404 response for non-numeric values in :id', (done) => {
+    request.get(`${API_URL}/cart/d200-44a5-9de6`, (_err, res, _body) => {
+      expect(res.statusCode).to.be.equal(404);
+      done();
+    });
+  });
 
-app.post('/login', (req, res) => {
-  const username = req.body.userName;
-  res.end(`Welcome ${username}`);
-});
+  it('POST /login returns valid response', (done) => {
+    request.post(`${API_URL}/login`, {json: {userName: 'Pinkbrook'}}, (_err, res, body) => {
+      expect(res.statusCode).to.be.equal(200);
+      expect(body).to.be.equal('Welcome Pinkbrook');
+      done();
+    });
+  });
 
-app.listen(port, () => {
-  console.log('API available on localhost port 7865');
+  it('GET /available_payments returns valid response', (done) => {
+    request.get(`${API_URL}/available_payments`, (_err, res, body) => {
+      expect(res.statusCode).to.be.equal(200);
+      expect(JSON.parse(body))
+        .to.be.deep.equal({payment_methods: {credit_cards: true, paypal: false}});
+      done();
+    });
+  });
 });
