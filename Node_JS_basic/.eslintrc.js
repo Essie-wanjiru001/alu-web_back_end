5f3378ad7648c751198f9c6b454bd738d2f92288
Essie-wanjiru001 <esthernjeriwanjiru287@gmail.com>
2024-07-27 23:01:15 +0300

    fixed lint errors

--- Node_JS_basic/.eslintrc.js
+++ Node_JS_basic/.eslintrc.js
@@ -1,38 +1,52 @@
 module.exports = {
-    env: {
-      browser: false,
-      es6: true,
-      jest: true,
-    },
-    extends: [
-      'airbnb-base',
-      'plugin:jest/all',
+  env: {
+    browser: false,
+    es6: true,
+    // Do not include 'jest: true' globally
+  },
+  extends: [
+    'airbnb-base',
+    // Ensure 'plugin:jest/all' is applied only to test files
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: ['jest'],
+  rules: {
+    'max-classes-per-file': 'off',
+    'no-underscore-dangle': 'off',
+    'no-console': 'off',
+    'no-shadow': 'off',
+    'no-restricted-syntax': [
+      'error',
+      'LabeledStatement',
+      'WithStatement',
     ],
-    globals: {
-      Atomics: 'readonly',
-      SharedArrayBuffer: 'readonly',
-    },
-    parserOptions: {
-      ecmaVersion: 2018,
-      sourceType: 'module',
-    },
-    plugins: ['jest'],
-    rules: {
-      'max-classes-per-file': 'off',
-      'no-underscore-dangle': 'off',
-      'no-console': 'off',
-      'no-shadow': 'off',
-      'no-restricted-syntax': [
-        'error',
-        'LabeledStatement',
-        'WithStatement',
+    'jest/require-hook': 'off', // Disable Jest hooks globally if not needed
+  },
+  overrides: [
+    {
+      files: ['*.test.js', '*.spec.js'], // Apply Jest rules only to test files
+      env: {
+        jest: true,
+      },
+      extends: [
+        'airbnb-base',
+        'plugin:jest/all',
       ],
+      rules: {
+        // Jest-specific rules
+        'jest/require-hook': 'error', // Ensure Jest hooks are required in test files
+      },
     },
-    overrides:[
-      {
-        files: ['*.js'],
-        excludedFiles: 'babel.config.js',
-      }
-    ]
-  };
-  
+    {
+      files: ['*.js'],
+      excludedFiles: '*.test.js', // Exclude test files from general rules
+    },
+  ],
+};
